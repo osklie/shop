0b72d27d3afdd104c985319838174d8fa2b009d2
oskar <oskarlietzau1@gmail.com>
2022-11-17 00:03:48 +0100

    first commit

---
+++ src/main/java/shop/project/ProjectApplication.java
@@ -0,0 +1,13 @@
+package shop.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import shop.project.domain.TaskDto;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/shop/project/domain/Task.java
@@ -0,0 +1,15 @@
+package shop.project.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+
+public class Task {
+    private String products;
+    private String groups;
+    private String carts;
+    private String orders;
+    private String users;
+}
